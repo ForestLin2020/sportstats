3c005abe058ece6b006692015e033e614528b498
zojobear <zoyilu@gmail.com>
2022-02-21 07:06:41 -0500

    finished Soccer box score and update volleyball box score

---
+++ src/components/SoccerBoxScore.vue
@@ -0,0 +1,252 @@
+<template>
+  <h2>Soccer Box Score</h2>
+  <div class="game-description" nowrap>
+    <!-- 2017 Concordia Irvine -->
+    <h6 style="text-align:left;"><strong v-if="stats.sogame.venue.location">Location:</strong> {{ stats.sogame.venue.location }} </h6>
+    <h6 style="text-align:left;"><strong v-if="stats.sogame.venue.time">Time:</strong> {{ stats.sogame.venue.time }} <strong>Date:</strong> {{ stats.sogame.venue.date }} </h6>
+    <h6 style="text-align:left;"><strong v-if="stats.sogame.venue.attend">Attendance:</strong> {{ stats.sogame.venue.attend }} </h6>
+  </div>
+
+  <div>
+    <h4>Goals By Period</h4>
+    <div class="table-responsive">
+      <table class="table-striped table-sm table-condensed table table-hover table-bordered total_up">
+        <thead>
+          <tr>
+              <th>University</th>
+              <th v-for="(len, index) in stats.sogame.plays.period.length" :key="index">{{len}}</th>
+              <th>Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(i, teamIndex) in 2" :key="teamIndex">
+            <td> {{stats.sogame.team[teamIndex].id}}</td>
+            <td v-for="(score, index) in stats.sogame.team[teamIndex].linescore.lineprd" :key="index">{{score.score}}</td>
+            <td>{{stats.sogame.team[teamIndex].linescore.score}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="mt-3" v-for="(i, teamIndex) in 2" :key="teamIndex">
+      <h6 style="text-align:left;"><strong>{{stats.sogame.team[teamIndex].name}}</strong> <small>{{stats.sogame.team[teamIndex].record}}</small></h6>
+      <div class="table-responsive">
+        <table class="table-striped table-sm table-condensed table table-hover table-bordered total_up">
+          <thead>
+            <tr>
+              <th>NO.</th>
+              <th style="text-align:left;">Athlete</th>
+              <th title="Goals">G</th>
+              <th title="Assists">A</th>
+              <th title="Shots">SH</th>
+              <th title="Shots On Goal">SOG</th>
+              <th title="Fouls">Fouls</th>
+              <th title="Minutes">MIN</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="player in playersInGameFilter(teamIndex)" :key="player.uni">
+              <td v-if="player.uni">{{player.uni}}</td><td v-else>0</td>
+              <td nowrap style="text-align:left;">{{player.name}}</td>
+              <td v-if="player.shots && player.shots.g">{{player.shots.g}}</td><td v-else>0</td>
+              <td v-if="player.shots && player.shots.a">{{player.shots.a}}</td><td v-else>0</td>
+              <td v-if="player.shots && player.shots.sh">{{player.shots.sh}}</td><td v-else>0</td>
+              <td v-if="player.shots && player.shots.sog">{{player.shots.sog}}</td><td v-else>0</td>
+              <td v-if="player.penalty && player.penalty.fouls">{{player.penalty.fouls}}</td><td v-else>0</td>
+              <td v-if="player.misc && player.misc.minutes">{{player.misc.minutes}}</td><td v-else>0</td>
+            </tr>
+            <tr>
+              <th>Totals</th>
+              <th></th>
+              <th>{{stats.sogame.team[teamIndex].totals.shots.g}}</th>
+              <th>{{stats.sogame.team[teamIndex].totals.shots.a}}</th>
+              <th>{{stats.sogame.team[teamIndex].totals.shots.sh}}</th>
+              <th>{{stats.sogame.team[teamIndex].totals.shots.sog}}</th>
+              <th>{{stats.sogame.team[teamIndex].totals.penalty.fouls}}</th>
+              <th></th>
+            </tr>
+          </tbody>
+          <thead>
+            <tr>
+              <th>NO.</th>
+              <th colspan="4" nowrap style="text-align:left;">Goalie</th>
+              <th title="Goals Allowed">GA</th>
+              <th>Saves</th>
+              <th>Minutes</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="player in goalkeeperInGameFilter(teamIndex)" :key="player.uni">
+              <td v-if="player.uni">{{player.uni}}</td><td v-else>0</td>
+              <td colspan="4" nowrap style="text-align:left;">{{player.name}}</td>
+              <td v-if="player.goalie && player.goalie.ga">{{player.goalie.ga}}</td><td v-else>0</td>
+              <td v-if="player.goalie && player.goalie.saves">{{player.goalie.saves}}</td><td v-else>0</td>
+              <td v-if="player.goalie && player.goalie.minutes">{{player.goalie.minutes}}</td><td v-else>0</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+
+  <div>
+    <h4>Saves By Period</h4>
+    <div class="table-responsive">
+      <table class="table-striped table-sm table-condensed table table-hover table-bordered total_up">
+        <thead>
+          <tr>
+              <th>University</th>
+              <th v-for="(len, index) in stats.sogame.plays.period.length" :key="index">{{len}}</th>
+              <th>Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(i, teamIndex) in 2" :key="teamIndex">
+            <td> {{stats.sogame.team[teamIndex].id}}</td>
+            <td v-for="(saves, index) in stats.sogame.team[teamIndex].linescore.lineprd" :key="index">{{saves.saves}}</td>
+            <td>{{stats.sogame.team[teamIndex].totals.goalie.saves}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+
+  <div>
+    <h4>Shots By Period</h4>
+    <div class="table-responsive">
+      <table class="table-striped table-sm table-condensed table table-hover table-bordered total_up">
+        <thead>
+          <tr>
+              <th>University</th>
+              <th v-for="(len, index) in stats.sogame.plays.period.length" :key="index">{{len}}</th>
+              <th>Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(i, teamIndex) in 2" :key="teamIndex">
+            <td> {{stats.sogame.team[teamIndex].id}}</td>
+            <td v-for="(shots, index) in stats.sogame.team[teamIndex].linescore.lineprd" :key="index">{{shots.shots}}</td>
+            <td>{{stats.sogame.team[teamIndex].totals.shots.sh}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+
+  <div>
+    <h4>Corner Kicks By Period</h4>
+    <div class="table-responsive">
+      <table class="table-striped table-sm table-condensed table table-hover table-bordered total_up">
+        <thead>
+          <tr>
+              <th>University</th>
+              <th v-for="(len, index) in stats.sogame.plays.period.length" :key="index">{{len}}</th>
+              <th>Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(i, teamIndex) in 2" :key="teamIndex">
+            <td> {{stats.sogame.team[teamIndex].id}}</td>
+            <td v-for="(corners, index) in stats.sogame.team[teamIndex].linescore.lineprd" :key="index">{{corners.corners}}</td>
+            <td>{{(stats.sogame.team[teamIndex].linescore.lineprd[0].corners * 1) + (stats.sogame.team[teamIndex].linescore.lineprd[1].corners * 1) }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+
+  <div>
+    <h4>Fouls By Period</h4>
+    <div class="table-responsive">
+      <table class="table-striped table-sm table-condensed table table-hover table-bordered total_up">
+        <thead>
+          <tr>
+              <th>University</th>
+              <th v-for="(len, index) in stats.sogame.plays.period.length" :key="index">{{len}}</th>
+              <th>Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(i, teamIndex) in 2" :key="teamIndex">
+            <td> {{stats.sogame.team[teamIndex].id}}</td>
+            <td v-for="(fouls, index) in stats.sogame.team[teamIndex].linescore.lineprd" :key="index">{{fouls.fouls}}</td>
+            <td>{{ stats.sogame.team[teamIndex].totals.penalty.fouls }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+
+  <div v-if="stats.sogame.scores">
+    <h4>Scoring Summary</h4>
+    <div class="table-responsive">
+      <table class="table-striped table-sm table-condensed table table-hover table-bordered total_up">
+        <!-- scores come with an array - start -->
+        <thead v-if="stats.sogame.scores.score && stats.sogame.scores.score.length">
+          <tr>
+            <th>No</th>
+            <th>Time</th>
+            <th style="text-align:left;">Goal</th>
+            <th>Description</th>
+          </tr>
+        </thead>
+        <tbody v-if="stats.sogame.scores.score && stats.sogame.scores.score.length">
+          <tr v-for="(score, teamIndex) in stats.sogame.scores.score" :key="teamIndex">
+            <td>{{score.number}}</td>
+            <td>{{score.time}}</td>
+            <td style="text-align:left;">Goal by {{score.name}} ({{score.id}})
+                <span v-if="score.assist1">, Assist By {{score.assist1}}</span>
+            </td>
+            <td>{{score.desc}}</td>
+          </tr>
+        </tbody>
+        <!-- scores come with an array - end -->
+        <!-- scores come with one object - start -->
+        <thead v-if="stats.sogame.scores.score && !stats.sogame.scores.score.length">
+          <tr>
+            <th>No</th>
+            <th>Time</th>
+            <th style="text-align:left;">Goal</th>
+          </tr>
+        </thead>
+        <tbody v-if="stats.sogame.scores.score && !stats.sogame.scores.score.length">
+          <tr>
+            <td>{{stats.sogame.scores.score.number}}</td>
+            <td>{{stats.sogame.scores.score.time}}</td>
+            <td colspan="2" style="text-align:left;">Goal by {{stats.sogame.scores.score.name}} ({{stats.sogame.scores.score.id}})
+                <span v-if="stats.sogame.scores.score.assist1">, Assist By {{stats.sogame.scores.score.assist1}}</span>
+            </td>
+          </tr>
+        </tbody>
+        <!-- scores come with one object - end -->
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SoccerScoreBox',
+  props: ['selected', 'stats'],
+  data () {
+    return {
+      totalsByYear: [],
+      careerTotals: []
+    }
+  },
+  methods: {
+    playersInGameFilter (teamIndex) {
+      return this.stats.sogame.team[teamIndex].player.filter(player => (player.gp !== '0' && player.uni !== 'TM')).sort(this.dateSort)
+    },
+    goalkeeperInGameFilter (teamIndex) {
+      return this.stats.sogame.team[teamIndex].player.filter(player => (player.pos === 'gk' || player.goalie)).sort(this.dateSort)
+    }
+  }
+}
+</script>
+
+<style scoped>
+thead {
+  color: white;
+  background: #0b5ed7;
+}
+</style>
--- src/components/VolleyballBoxScore.vue
+++ src/components/VolleyballBoxScore.vue
@@ -7,112 +7,108 @@
     <h6 style="text-align:left;"><strong v-if="stats.vbgame.venue.attend">Attendance:</strong> {{ stats.vbgame.venue.attend }} </h6>
   </div>
 
-  <div class="table-responsive">
-    <table class="table-striped table-sm table-condensed table table-hover table-bordered total_up">
-      <thead>
-        <tr>
-          <th nowrap>Set Scores</th>
-          <th>1</th>
-          <th>2</th>
-          <th>3</th>
-          <th v-if="stats.vbgame.breakdowns[0].breakdown[3]">4</th>
-          <th v-if="stats.vbgame.breakdowns[0].breakdown[4]">5</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td >{{stats.vbgame.team[0].id}} </td>
-          <td >{{stats.vbgame.team[0].linescore.linegame[0].points}}</td>
-          <td >{{stats.vbgame.team[0].linescore.linegame[1].points}}</td>
-          <td >{{stats.vbgame.team[0].linescore.linegame[2].points}}</td>
-          <td v-if="stats.vbgame.team[0].linescore.linegame[3]">{{stats.vbgame.team[0].linescore.linegame[3].points}}</td>
-          <td v-if="stats.vbgame.team[0].linescore.linegame[4]">{{stats.vbgame.team[0].linescore.linegame[4].points}}</td>
-        </tr>
-        <tr>
-          <td >{{stats.vbgame.team[1].id}} </td>
-          <td >{{stats.vbgame.team[1].linescore.linegame[0].points}}</td>
-          <td >{{stats.vbgame.team[1].linescore.linegame[1].points}}</td>
-          <td >{{stats.vbgame.team[1].linescore.linegame[2].points}}</td>
-          <td v-if="stats.vbgame.team[1].linescore.linegame[3]">{{stats.vbgame.team[1].linescore.linegame[3].points}}</td>
-          <td v-if="stats.vbgame.team[1].linescore.linegame[4]">{{stats.vbgame.team[1].linescore.linegame[4].points}}</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-
-  <div v-for="(i, teamIndex) in 2" :key="teamIndex">
-    <h6 style="text-align:left;"><strong>{{stats.vbgame.team[teamIndex].name}}</strong> <small>{{stats.vbgame.team[teamIndex].record}}</small></h6>
+  <div>
+    <h4>Box Score</h4>
     <div class="table-responsive">
       <table class="table-striped table-sm table-condensed table table-hover table-bordered total_up">
         <thead>
           <tr>
-            <th>NO.</th>
-            <th style="text-align:left;">Athlete</th>
-            <th title="Sets">S</th>
-            <th title="Kills">K</th>
-            <th title="Errors">E</th>
-            <th title="Total Attemps">TA</th>
-            <th title="Hitting Pecentage">Pct</th>
-            <th title="Assists">Ast</th>
-            <th title="Service Aces">SA</th>
-            <th title="Service Errors">SE</th>
-            <th title="Reception Errors">RE</th>
-            <th title="Digs">Digs</th>
-            <th title="Block Solos">BS</th>
-            <th title="Block Assisted">BA</th>
-            <th title="Block Errors">BE</th>
-            <th title="Ball Handling Errors">BHE</th>
-            <th title="Points">Pts</th>
+            <th nowrap>Set Scores</th>
+            <th>1</th>
+            <th>2</th>
+            <th>3</th>
+            <th v-if="stats.vbgame.breakdowns[0].breakdown[3]">4</th>
+            <th v-if="stats.vbgame.breakdowns[0].breakdown[4]">5</th>
           </tr>
         </thead>
         <tbody>
-          <tr v-for="player in playersInGameFilter(teamIndex)" :key="player.uni">
-            <td v-if="player.uni">{{player.uni}}</td><td v-else>0</td>
-            <td nowrap style="text-align:left;">{{player.name}}</td>
-            <td v-if="player.gp">{{player.gp}}</td><td v-else>0</td>
-            <td v-if="player.attack && player.attack.k">{{player.attack.k}}</td><td v-else>0</td>
-            <td v-if="player.attack && player.attack.e">{{player.attack.e}}</td><td v-else>0</td>
-            <td v-if="player.attack && player.attack.ta">{{player.attack.ta}}</td><td v-else>0</td>
-            <td v-if="player.attack && player.attack.pct">{{player.attack.pct}}</td><td v-else>0</td>
-            <td v-if="player.set && player.set.a">{{player.set.a}}</td><td v-else>0</td>
-            <td v-if="player.serve && player.serve.sa">{{player.serve.sa}}</td><td v-else>0</td>
-            <td v-if="player.serve && player.serve.se">{{player.serve.se}}</td><td v-else>0</td>
-            <td v-if="player.defense && player.defense.re">{{player.defense.re}}</td><td v-else>0</td>
-            <td v-if="player.defense && player.defense.dig">{{player.defense.dig}}</td><td v-else>0</td>
-            <td v-if="player.block && player.block.bs">{{player.block.bs}}</td><td v-else>0</td>
-            <td v-if="player.block && player.block.ba">{{player.block.ba}}</td><td v-else>0</td>
-            <td v-if="player.block && player.block.be">{{player.block.be}}</td><td v-else>0</td>
-            <td v-if="player.misc && player.misc.bhe">{{player.misc.bhe}}</td><td v-else>0</td>
-            <td v-if="player.misc && player.misc.pts">{{player.misc.pts}}</td><td v-else>0</td>
-          </tr>
-          <tr>
-            <th></th>
-            <th style="text-align:left">Totals</th>
-            <th></th>
-            <th>{{stats.vbgame.team[teamIndex].totals.attack.k}}</th>
-            <th>{{stats.vbgame.team[teamIndex].totals.attack.e}}</th>
-            <th>{{stats.vbgame.team[teamIndex].totals.attack.ta}}</th>
-            <th>{{stats.vbgame.team[teamIndex].totals.attack.pct}}</th>
-            <th>{{stats.vbgame.team[teamIndex].totals.set.a}}</th>
-            <th>{{stats.vbgame.team[teamIndex].totals.serve.sa}}</th>
-            <th>{{stats.vbgame.team[teamIndex].totals.serve.se}}</th>
-            <th>{{stats.vbgame.team[teamIndex].totals.defense.re}}</th>
-            <th>{{stats.vbgame.team[teamIndex].totals.defense.dig}}</th>
-            <th>{{stats.vbgame.team[teamIndex].totals.block.bs}}</th>
-            <th>{{stats.vbgame.team[teamIndex].totals.block.ba}}</th>
-            <th>{{stats.vbgame.team[teamIndex].totals.block.be}}</th>
-            <th>{{stats.vbgame.team[teamIndex].totals.misc.bhe}}</th>
-            <th>{{stats.vbgame.team[teamIndex].totals.misc.pts}}</th>
+          <tr v-for="(i, teamIndex) in 2" :key="teamIndex">
+            <td >{{stats.vbgame.team[teamIndex].id}} </td>
+            <td >{{stats.vbgame.team[teamIndex].linescore.linegame[0].points}}</td>
+            <td >{{stats.vbgame.team[teamIndex].linescore.linegame[1].points}}</td>
+            <td >{{stats.vbgame.team[teamIndex].linescore.linegame[2].points}}</td>
+            <td v-if="stats.vbgame.team[teamIndex].linescore.linegame[3]">{{stats.vbgame.team[teamIndex].linescore.linegame[3].points}}</td>
+            <td v-if="stats.vbgame.team[teamIndex].linescore.linegame[4]">{{stats.vbgame.team[teamIndex].linescore.linegame[4].points}}</td>
           </tr>
         </tbody>
       </table>
     </div>
+
+    <div class="mt-3" v-for="(i, teamIndex) in 2" :key="teamIndex">
+      <h6 style="text-align:left;"><strong>{{stats.vbgame.team[teamIndex].name}}</strong> <small>{{stats.vbgame.team[teamIndex].record}}</small></h6>
+      <div class="table-responsive">
+        <table class="table-striped table-sm table-condensed table table-hover table-bordered total_up">
+          <thead>
+            <tr>
+              <th>NO.</th>
+              <th style="text-align:left;">Athlete</th>
+              <th title="Sets">S</th>
+              <th title="Kills">K</th>
+              <th title="Errors">E</th>
+              <th title="Total Attemps">TA</th>
+              <th title="Hitting Pecentage">Pct</th>
+              <th title="Assists">Ast</th>
+              <th title="Service Aces">SA</th>
+              <th title="Service Errors">SE</th>
+              <th title="Reception Errors">RE</th>
+              <th title="Digs">Digs</th>
+              <th title="Block Solos">BS</th>
+              <th title="Block Assisted">BA</th>
+              <th title="Block Errors">BE</th>
+              <th title="Ball Handling Errors">BHE</th>
+              <th title="Points">Pts</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="player in playersInGameFilter(teamIndex)" :key="player.uni">
+              <td v-if="player.uni">{{player.uni}}</td><td v-else>0</td>
+              <td nowrap style="text-align:left;">{{player.name}}</td>
+              <td v-if="player.gp">{{player.gp}}</td><td v-else>0</td>
+              <td v-if="player.attack && player.attack.k">{{player.attack.k}}</td><td v-else>0</td>
+              <td v-if="player.attack && player.attack.e">{{player.attack.e}}</td><td v-else>0</td>
+              <td v-if="player.attack && player.attack.ta">{{player.attack.ta}}</td><td v-else>0</td>
+              <td v-if="player.attack && player.attack.pct">{{player.attack.pct}}</td><td v-else>0</td>
+              <td v-if="player.set && player.set.a">{{player.set.a}}</td><td v-else>0</td>
+              <td v-if="player.serve && player.serve.sa">{{player.serve.sa}}</td><td v-else>0</td>
+              <td v-if="player.serve && player.serve.se">{{player.serve.se}}</td><td v-else>0</td>
+              <td v-if="player.defense && player.defense.re">{{player.defense.re}}</td><td v-else>0</td>
+              <td v-if="player.defense && player.defense.dig">{{player.defense.dig}}</td><td v-else>0</td>
+              <td v-if="player.block && player.block.bs">{{player.block.bs}}</td><td v-else>0</td>
+              <td v-if="player.block && player.block.ba">{{player.block.ba}}</td><td v-else>0</td>
+              <td v-if="player.block && player.block.be">{{player.block.be}}</td><td v-else>0</td>
+              <td v-if="player.misc && player.misc.bhe">{{player.misc.bhe}}</td><td v-else>0</td>
+              <td v-if="player.misc && player.misc.pts">{{player.misc.pts}}</td><td v-else>0</td>
+            </tr>
+            <tr>
+              <th></th>
+              <th style="text-align:left">Totals</th>
+              <th></th>
+              <th>{{stats.vbgame.team[teamIndex].totals.attack.k}}</th>
+              <th>{{stats.vbgame.team[teamIndex].totals.attack.e}}</th>
+              <th>{{stats.vbgame.team[teamIndex].totals.attack.ta}}</th>
+              <th>{{stats.vbgame.team[teamIndex].totals.attack.pct}}</th>
+              <th>{{stats.vbgame.team[teamIndex].totals.set.a}}</th>
+              <th>{{stats.vbgame.team[teamIndex].totals.serve.sa}}</th>
+              <th>{{stats.vbgame.team[teamIndex].totals.serve.se}}</th>
+              <th>{{stats.vbgame.team[teamIndex].totals.defense.re}}</th>
+              <th>{{stats.vbgame.team[teamIndex].totals.defense.dig}}</th>
+              <th>{{stats.vbgame.team[teamIndex].totals.block.bs}}</th>
+              <th>{{stats.vbgame.team[teamIndex].totals.block.ba}}</th>
+              <th>{{stats.vbgame.team[teamIndex].totals.block.be}}</th>
+              <th>{{stats.vbgame.team[teamIndex].totals.misc.bhe}}</th>
+              <th>{{stats.vbgame.team[teamIndex].totals.misc.pts}}</th>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   </div>
+
   <div class="mt-3">
     <h4>Team Attack Stats By Set</h4>
-    <div v-for="(i, teamIndex) in 2" :key="teamIndex">
+    <div class="mt-3" v-for="(i, teamIndex) in 2" :key="teamIndex">
       <h6 style="text-align:left;"><strong>{{stats.vbgame.team[teamIndex].name}}</strong> <small>{{stats.vbgame.team[teamIndex].linescore.score}} sets {{stats.vbgame.breakdowns[teamIndex].block}} blocks</small> </h6>
-      <div class="table-responsive mt-3">
+      <div class="table-responsive">
         <table class=" table-striped table-sm table-condensed table table-hover table-bordered total_up">
           <thead>
             <tr>
